a696072e625e83bd5722bb0bd4d7102e1698642c
lexer <alexey.v.zaharov@gmail.com>
2011-04-25 01:56:05 +0600

    static files routing

--- proxy.js
+++ proxy.js
@@ -1,6 +1,24 @@
 var httpProxy = require('http-proxy');
-    
+   
 //
 // Http Proxy Server with Proxy Table
 //
-httpProxy.createServer(3000, 'localhost').listen(8000);
+httpProxy.createServer(9797, 'localhost').listen(8000);
+
+var railsProxy = new httpProxy.HttpProxy();
+
+function proxyRails(req, res, next){
+  railsProxy.proxyRequest(req, res, {
+    host: 'localhost',
+    port: 3000
+  });
+}
+
+var connect = require('connect');
+
+var server = connect.createServer();
+
+server.use(connect.static(__dirname + '/public'));
+server.use(proxyRails);
+
+server.listen(9797);
